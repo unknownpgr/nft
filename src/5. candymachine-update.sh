c84a3a43ffcdb55e0dc17e4a904eb4dc340337ec
unknownpgr <unknownpgr@gmail.com>
2021-12-17 01:03:12 +0000

    Add some scripts

---
+++ src/5. candymachine-update.sh
@@ -0,0 +1,6 @@
+ts-node ~/metaplex-foundation/metaplex/js/packages/cli/src/candy-machine-cli.ts \
+  update_candy_machine \
+  --env devnet \
+  --keypair .config/solana/devnet.json \
+  -p 1 \
+  --date "$(date)"
---
+++ src/5. mint_token.sh
@@ -0,0 +1,1 @@
+ts-node ~/metaplex-foundation/metaplex/js/packages/cli/src/candy-machine-cli.ts mint_one_token -k .config/solana/devnet.json --env devnet
