732ed88cc01dea8b38584e1f5b742133085eb885
unknownpgr <unknownpgr@gmail.com>
2021-12-17 01:26:33 +0000

    Update

--- src/5. candymachine-update.sh
+++ src/5. candymachine-update.sh
@@ -2,5 +2,5 @@ ts-node ~/metaplex-foundation/metaplex/js/packages/cli/src/candy-machine-cli.ts 
   update_candy_machine \
   --env devnet \
   --keypair .config/solana/devnet.json \
-  -p 1 \
+  -p 10 \
   --date "$(date)"
