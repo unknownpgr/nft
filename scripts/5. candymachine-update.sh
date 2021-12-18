3f2813a8d45b7dcbc584a3b5bdda2dae064203da
unknownpgr <unknownpgr@gmail.com>
2021-12-18 21:32:37 +0000

    Move scripts

--- scripts/5. candymachine-update.sh
+++ scripts/5. candymachine-update.sh
@@ -2,5 +2,5 @@ ts-node ~/metaplex-foundation/metaplex/js/packages/cli/src/candy-machine-cli.ts 
   update_candy_machine \
   --env devnet \
   --keypair .config/solana/devnet.json \
-  -p 10 \
+  -p 1 \
   --date "$(date)"
