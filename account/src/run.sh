b51d47ccd4e77ea454058fc951a55e5f758b3a8e
unknownpgr <unknownpgr@gmail.com>
2021-12-18 21:44:17 +0000

    Simplify run.sh

--- account/src/run.sh
+++ account/src/run.sh
@@ -1,4 +1,3 @@
 solana-keygen new --outfile secret.json > output.txt 2> /dev/null
 python account.py
-solana config set --keypair secret.json > /dev/null
-solana airdrop 1 > /dev/null
+solana airdrop 1 --keypair secret.json > /dev/null
